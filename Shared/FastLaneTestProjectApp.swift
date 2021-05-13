4b2f3a8f391fd0db0c0d9a1246d176caaa7d47cd
Sangeeth Sivakumar <05sangy@gmail.com>
2021-05-13 16:48:50 +0800

    First Commit

---
+++ Shared/FastLaneTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastLaneTestProjectApp.swift
+//  Shared
+//
+//  Created by Sangy-Shrabo on 12/5/21.
+//
+
+import SwiftUI
+
+@main
+struct FastLaneTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
